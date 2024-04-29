ed54ddbce490ceb56b73d1ecf6b02f7613c29d8a
IssacPachecoEk <nolbertoestrella18@hotmail.com>
2024-04-28 19:32:03 -0600

    sonar intento 2

--- api/handler.js
+++ api/handler.js
@@ -6,7 +6,7 @@ async function getAppConfig(url) {
       if (res.statusCode < 200 || res.statusCode >= 300) {
         return reject(new Error("statusCode=" + res.statusCode));
       }
-      var body = [];
+      let body = [];
       res.on("data", function (chunk) {
         body.push(chunk);
       });
